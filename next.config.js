c53d4ea5b90eb864ed4db7ae29e16e9ed9c63a5d
andre faria <andre.faria@ceiia.com>
2023-08-01 12:03:41 +0100

    new

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+}
+
+module.exports = nextConfig
+
+// lista de packages que ja vem no bundle do next13
+// https://nextjs.org/docs/app/api-reference/next-config-js/serverComponentsExternalPackages
---
+++ readme.md
@@ -0,0 +1,7 @@
+# sort imports
+# imutability 
+# auth service
+# 
+  //https://stackoverflow.com/questions/34817617/should-jwt-be-stored-in-localstorage-or-cookie
+
+# edge runtime
